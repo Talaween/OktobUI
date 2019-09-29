0a8e41fb5bc8b72f6258082a261a1e338a9dbfff
Mahmoud Awad <awad@talaween.net>
2019-09-29 16:35:56 +0100

    added create tables

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 //import koa
 var Koa = require('koa');
-//import koa-router which is used to route user request to its path
-var Router = require('koa-router');
-//import koa-body parser whic his used to extract parameters from requests
+
+//import all the routes
+var welcome = require('./routes/welcome');
+var admin = require('./routes/admin');
+var articles = require('./routes/articles.js');
 
 //create a koa instance and store it in app variable
 var app = new Koa();
 
-var router = new Router();
-
-router.get('/api/v1.0', welcomeAPI);
-
-//use the root routes
-app.use(router.routes());
-
-//import the Router we defined in articles.js
-var articles = require('./routes/articles.js');
-
-//apply the routes as a middleware
+////apply the routes as a middleware
+app.use(welcome.routes());
+app.use(admin.routes());
 app.use(articles.routes());
 
-//run the werver on port 3000
-app.listen(3000);
+//if there is no environment variable set for port number
+//use a default value of 3000
+var port = process.env.PORT || 3000;
 
-function welcomeAPI(cnx, next){
-    console.log("yes")
-    cnx.body = {message:'Welcome to Oktob API version 1.0'};
-}
+//run the werver on port 3000
+app.listen(port);
+
